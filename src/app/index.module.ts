2d80314704bca24e863aae27248e4f987aeb5cdb
Lialosiu <lialosiu@gmail.com>
2016-05-22 13:11:51 +0800

    fix reference path

--- src/app/index.module.ts
+++ src/app/index.module.ts
@@ -1,4 +1,4 @@
-/// <reference path="../../typings/main.d.ts" />
+/// <reference path="../../typings/index.d.ts" />
 
 import { config } from './index.config';
 import { routerConfig } from './index.route';
