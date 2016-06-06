17f6bb4a9f95ffb802b3bf5625581ffcd938dee4
Lialosiu <lialosiu@gmail.com>
2016-06-06 13:26:03 +0800

    样式

--- src/app/index.run.ts
+++ src/app/index.run.ts
@@ -1,11 +1,17 @@
 import {YuaInfoService} from "./service/YuaInfoService";
 import {IRootScopeService} from "./interface/Yua/IRootScopeService";
 import {YuaPostService} from "./service/YuaPostService";
+import IStateService = angular.ui.IStateService;
 
 /** @ngInject */
-export function runBlock($log: angular.ILogService, $rootScope: IRootScopeService, $_YuaInfo: YuaInfoService, $_YuaPost: YuaPostService) {
+export function runBlock($log: angular.ILogService,
+                         $rootScope: IRootScopeService,
+                         $_YuaInfo: YuaInfoService,
+                         $_YuaPost: YuaPostService,
+                         $state: IStateService) {
     $log.debug('runBlock end');
 
     $rootScope.$_YuaInfo = $_YuaInfo;
     $rootScope.$_YuaPost = $_YuaPost;
+    $rootScope.$state = $state;
 }
--- src/app/interface/Yua/IRootScopeService.ts
+++ src/app/interface/Yua/IRootScopeService.ts
@@ -1,7 +1,9 @@
 import {YuaInfoService} from "../../service/YuaInfoService";
 import {YuaPostService} from "../../service/YuaPostService";
+import IStateService = angular.ui.IStateService;
 
 export interface IRootScopeService extends angular.IRootScopeService {
     $_YuaInfo: YuaInfoService;
     $_YuaPost: YuaPostService;
+    $state: IStateService;
 }
